a34f226d55089c4999cdc43d9ca1f5d9284deaa2
Eric Francis <airdrik@gmail.com>
2011-04-21 13:27:45 -0600

    add fromCursor method which creates DataTables from the results stored in the cursor

--- datatable.py
+++ datatable.py
@@ -50,6 +50,50 @@ def fromXML(s):
 	x = myxml.XmlNode(s)
 	return DataTable(row.attributes() for row in x.row)
 
+def fromCursor(cur, scrub=None):
+	'''Expects cur to be a pysql 2.0 - style cursor and returns a (list of) DataTable(s) with the results
+	optional parameter scrub is a method which is called for each header (row from cursor.description) to return a replace method 
+		which is then called on each value for that header
+		return None to do nothing on that header
+	
+	example - using adodbapi to connect to MS SQL server, the following will normalize smalldatetime fields to date objects and datetime fields to datetime objects:
+	
+	def parseCursor(cursor):
+		def scrub(header):
+			if header[1] == 135 and header[5] == 0: #135 is the sql datetime type, header[5] is the size of the field
+				def toDate(dt):
+					if isinstance(dt, datetime.datetime):
+						return dt.date()
+					return dt
+				return toDate
+			elif header[1] == 135:
+				def toDateTime(dt):
+					if isinstance(dt, datetime.date) and not isinstance(dt, datetime.datetime):
+						return datetime.datetime(dt.year, dt.month, dt.day)
+					return dt
+				return toDateTime
+			return None
+		return fromCursor(cursor, scrub)
+	'''
+	if not cur.description:
+		return DataTable()
+	def result():
+		headers = [h[0] for h in cur.description]
+		theData = [AttributeDict(zip((h for h in headers), row)) for row in cur.fetchall()]
+		if scrub is not None:
+			for desc in cur.description:
+				replace = scrub(desc)
+				if replace is not None:
+					for row in theData:
+						row[desc[0]] = replace(row[desc[0]])
+		return DataTable(theData)
+	results = [result()]
+	while cur.nextset():
+		results.append(result())
+	if len(results) == 1:
+		return results[0]
+	return results
+
 def first(it):
 	try:
 		return it.next()
